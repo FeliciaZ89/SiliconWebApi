a89a46d7d968bf0cd6e4cdce92748e9b1639c5b6
felyc <fely.coman@gmail.com>
2024-04-17 11:16:06 +0200

    added CourseEntity, database new table

--- Infrastructure/Contexts/ApiContext.cs
+++ Infrastructure/Contexts/ApiContext.cs
@@ -7,5 +7,7 @@ namespace Infrastructure.Contexts;
 public class ApiContext(DbContextOptions<ApiContext> options) : DbContext(options)
 {
     public DbSet<SubscribersEntity> Subscribes { get; set; }
+    public DbSet<CourseEntity> Courses { get; set; }
+
 
 }

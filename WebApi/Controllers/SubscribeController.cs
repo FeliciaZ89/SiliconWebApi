ecdc64571f958c456a4af6378de0fd3a98786310
felyc <fely.coman@gmail.com>
2024-04-17 09:54:06 +0200

    Added infrastructure/controllers

---
+++ WebApi/Controllers/SubscribeController.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SubscribeController : ControllerBase
+{
+    public async Task<IActionResult> Subscribe()
+    {
+        return Ok();
+    }
+
+    public async Task<IActionResult> Unsubscribe()
+    {
+        return Ok();
+    }
+}

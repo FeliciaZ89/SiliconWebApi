fa42b9415e4e93c52a368154c2967631f16419d6
felyc <fely.coman@gmail.com>
2024-04-17 10:56:40 +0200

    post/delete-to subscribe done

--- WebApi/Controllers/SubscribeController.cs
+++ WebApi/Controllers/SubscribeController.cs
@@ -1,21 +1,53 @@
 ﻿
+using Infrastructure.Contexts;
+using Infrastructure.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class SubscribeController : ControllerBase
+public class SubscribeController(ApiContext context) : ControllerBase
+
+
 {
-    [HttpGet]
+    private readonly ApiContext _context = context;
+
+    [HttpPost]
     public async Task<IActionResult> Subscribe(string email)
     {
-        return Ok();
+        if (ModelState.IsValid)
+        {
+            if (await _context.Subscribes.AnyAsync(x => x.Email == email))
+                return Conflict();
+
+            _context.Add(new SubscribersEntity { Email = email });
+            await _context.SaveChangesAsync();
+            return Ok();
+
+        }
+
+
+        return BadRequest();
     }
 
     [HttpDelete]
     public async Task<IActionResult> Unsubscribe(string email)
     {
-        return Ok();
+        if (ModelState.IsValid)
+        {
+
+            var subscribersEntity = await _context.Subscribes.FirstOrDefaultAsync(x => x.Email == email);
+            if (subscribersEntity == null)
+                return NotFound();
+
+            _context.Remove(subscribersEntity);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+
+        return BadRequest();
     }
 }
